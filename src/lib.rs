be4cdd213fa317792a21605e8e8ca1e9dbfa6a1c
Bart Massey <bart.massey@gmail.com>
2021-03-13 16:18:46 -0800

    made ConvertError std::error::Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,82 @@
+/*!
+This Rust crate builds on the excellent work of the
+`encoding_rs`, `codepage`, and `oem-cp` crates in an attempt
+to provide idiomatic encoding and decoding of strings coded
+according to
+[Windows code pages](https://en.wikipedia.org/wiki/Windows_code_page).
+
+Because Windows code pages are a legacy rathole, it is
+difficult to transcode strings using them. Sadly, there are
+still a lot of files out there that use these encodings.
+This crate was specifically created for use with [RIFF](), a
+file format that has code pages baked in for text
+internationalization.
+
+No effort has been made to deal with Windows code pages
+beyond those supported by `codepage` and `oem-cp`. If the
+codepage you need is missing, I suggest taking a look at
+adding it to `oem-cp`, which seems to be the main Rust
+repository for unusual Windows code page tables. I believe
+that most of the code pages supported by `iconv` are dealt
+with here, but I haven't checked carefully.
+
+No particular effort has been put into performance. The
+interface allows `std::borrow::Cow` to some extent, but this
+is limited by the minor impedance mismatches between
+`encoding_rs` and `oem-cp`.
+
+# Examples
+
+Do some string conversions on Windows code page 869
+(alternate Greek).
+
+```rust
+# use codepage_strings::*;
+
+let coding = Coding::new(869).unwrap();
+assert_eq!(
+    coding.encode("αβ").unwrap(),
+    vec![214, 215],
+);
+assert_eq!(
+    coding.decode(&[214, 215]).unwrap(),
+    "αβ",
+);
+assert_eq!(
+    coding.decode_lossy(&[214, 147]),
+    "α\u{fffd}",
+);
+assert_eq!(
+    coding.decode(&[214, 147]),
+    Err(ConvertError::StringDecoding),
+);
+```
+*/
+
 use std::borrow::Cow;
 
 #[non_exhaustive]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ConvertError {
-    Range,
     StringEncoding,
     StringDecoding,
     CodePage,
 }
 
+impl std::fmt::Display for ConvertError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let msg = match self {
+            ConvertError::StringEncoding => "string codepage encoding error",
+            ConvertError::StringDecoding => "string decoding error",
+            ConvertError::CodePage => "invalid / unknown Windows code page",
+        };
+        write!(f, "{}", msg)
+    }
+}
+
+impl std::error::Error for ConvertError {}
+
+#[derive(Debug, Clone)]
 enum Codings {
     ERS(&'static encoding_rs::Encoding),
     OCC {
@@ -16,6 +85,7 @@ enum Codings {
     },
 }
 
+#[derive(Debug, Clone)]
 pub struct Coding(Codings);
 
 impl Coding {
