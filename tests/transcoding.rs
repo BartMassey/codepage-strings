6898e8b2937e04b883515230a728dbd47435d5f5
Bart Massey <bart.massey@gmail.com>
2021-03-13 17:48:19 -0800

    added more tests

--- tests/transcoding.rs
+++ tests/transcoding.rs
@@ -1,7 +1,32 @@
+use codepage_strings::*;
+
+// Do some tests on Windows code page 869 (alternate Greek),
+// which is encoded by `oem_cp`. XXX This test is duplicated
+// as an example in the crate doctests.
+
+#[test]
+fn test_oem_cp() {
+    let coding = Coding::new(869).unwrap();
+    assert_eq!(
+        coding.encode("αβ").unwrap(),
+        vec![214, 215],
+    );
+    assert_eq!(
+        coding.decode(&[214, 215]).unwrap(),
+        "αβ",
+    );
+    assert_eq!(
+        coding.decode_lossy(&[214, 147]),
+        "α\u{fffd}",
+    );
+    assert_eq!(
+        coding.decode(&[214, 147]),
+        Err(ConvertError::StringDecoding),
+    );
+}
+
 // Do some tests on Windows code page 1257 (Baltic),
 // which is encoded by `encoding_rs`.
-
-use codepage_strings::*;
 
 #[test]
 fn test_encoding_rs() {
@@ -20,6 +45,30 @@ fn test_encoding_rs() {
     );
     assert_eq!(
         coding.decode(&[224, 161]),
+        Err(ConvertError::StringDecoding),
+    );
+}
+
+// Do some tests on UTF-8, which is identity-encoded.
+
+#[test]
+fn test_identity() {
+    let coding = Coding::new(65001).unwrap();
+    let baltic = coding.encode("ąž").unwrap();
+    assert_eq!(
+        baltic,
+        "ąž".as_bytes(),
+    );
+    assert_eq!(
+        coding.decode(&baltic).unwrap(),
+        "ąž",
+    );
+    assert_eq!(
+        coding.decode_lossy(&[65, 255]),
+        "A\u{fffd}",
+    );
+    assert_eq!(
+        coding.decode(&[65, 255]),
         Err(ConvertError::StringDecoding),
     );
 }
